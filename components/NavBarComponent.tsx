dec713fe4e365266f7ae6092841c7fc37b5e6636
alskaf1293 <48730163+alskaf1293@users.noreply.github.com>
2023-06-17 22:28:56 -0700

    fixed posts

--- components/NavBarComponent.tsx
+++ components/NavBarComponent.tsx
@@ -8,7 +8,7 @@ const NavBarComponent = () => {
         <a href = "/home" style={{position: 'relative', left: 50, top: 5}}>
             <img src={logo.src} alt = "Placeholder" className='max-w-full max-h-full'/>
         </a>
-        <a href ="/Posts" className="absolute left-[222px] top-[30px] text-xl text-left text-white">Publish</a>
+        <a href ="/posts" className="absolute left-[222px] top-[30px] text-xl text-left text-white">Publish</a>
         <a href ="/profile" className="absolute left-[346px] top-[30px] text-xl text-left text-white">Profile</a>
     </div>
     )
