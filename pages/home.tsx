cd3ad6787fe6b4f4cdd8c1004245bdf77ec9c131
RiyanaD <riyana.duttaa@gmail.com>
2023-06-18 03:16:10 -0700

    stashing to pull

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,6 +5,7 @@ import { Database } from '../types/database'
 import styles from "../styles/Styles.module.css"
 import { GetServerSidePropsContext } from 'next'
 import NavBarComponent from '@/components/NavBarComponent'  // imports the nav bar
+import axios from 'axios';
 
 type Post = {
   id: number,
@@ -13,7 +14,8 @@ type Post = {
   authors: string[],
   content: string,
   pdf: string,
-  embedding: number[]
+  embedding: number[],
+  summary: string
 }
 
 export default function Home({ user }: { user: User }) {
@@ -29,9 +31,39 @@ export default function Home({ user }: { user: User }) {
       .from('posts')
       .select('*')
   
+      const summarizes = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+            model: 'gpt-4',
+            messages: [
+                {
+                    role: "system",
+                    content: "This is a chat that a user had with a chatbot. Describe the user in as many descriptive sentences as you can, but do so from the first-person point of view. For example, I am very interested in video games. could be one description. Every unique description should be a sentence, and every sentence should represent a unique aspect of the user. End every sentence with a newline."
+                },
+                {
+                    role: "user",
+                    content: posts.content
+                }
+            ],
+            max_tokens: 200,
+            n: 1,
+            stop: null,
+            temperature: 0,
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer `  + process.env.NEXT_PUBLIC_API_KEY,
+            },
+        }
+      )
+      setSummary(summarizes.data.choices[0].message.content)
+    };
+
     if (error) console.log('Error fetching posts: ', error)
     else {
       const formattedData: Post[] = data.map(post => ({
+        const summarizedContent = await summarize(post.content);
         id: Number(post.post_id),
         created_at: new Date().toISOString(), // You'll need to adjust this if you have a specific date to assign
         title: post.title,
@@ -42,7 +74,6 @@ export default function Home({ user }: { user: User }) {
       }))
       setPosts(formattedData)
     }
-  }
 
   return (
       <>
@@ -60,6 +91,7 @@ export default function Home({ user }: { user: User }) {
             <h2 className={styles.title}>{post.title}</h2>
             <h3 className={styles.author}>Author: {post.authors.join(', ')}</h3>
             <p className={styles.content}>{post.content}</p>
+            <p className={styles.content}>{post.summary}</p>
             <a href={post.pdf} className={styles.link}>View PDF</a>
             {/* Since embedding is a float array, just for illustration here. You might want to do something else with this data */}
             {post.embedding && <p>{post.embedding.join(', ')}</p>}
